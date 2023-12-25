6001c2f07a31cf30b2ec1d78abbdd3044b38e340
Kenny <72945813+lauk20@users.noreply.github.com>
2023-12-25 14:48:09 -0500

    add surface normal for the sphere, tested using normal to determine ray color at sphere-ray hit point

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 #include "vec3.h"
 
 /*
-    determine whether a ray hits a sphere.
+    determine whether a ray hits a sphere and return the parameter
+    t of the ray equation P = A + Bt that gives the closest hit point
     Derived from the equation (x-C_x)^2 + (y-C_y)^2 + (z-C_z)^2 = r^2
     and then representing the equation using vectors where the center is
     C = (C_x, C_y, C_z) and the point on the sphere is P = (x, y, z).
@@ -19,15 +20,21 @@
     @param radius radius of the sphere
     @param r ray to determine if it hits the sphere
 
-    @return true if the ray hits the sphere, false otherwise
+    @return parameter t of P = A + Bt of the closest hit point,
+            -1 if the ray does not hit the sphere.
 */
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
+double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 center_to_origin = r.origin() - center;
-    double a = dot(r.direction(), r.direction());
-    double b = 2.0 * dot(r.direction(), center_to_origin);
-    double c = dot(center_to_origin, center_to_origin) - radius * radius;
-    double discriminant = b * b - 4 * a * c;
-    return (discriminant >= 0);
+    double a = r.direction().length_squared();
+    double half_b = dot(r.direction(), center_to_origin);
+    double c = center_to_origin.length_squared() - radius * radius;
+    double discriminant = half_b * half_b - a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-half_b - std::sqrt(discriminant)) / a;
+    }
 }
 
 /*
@@ -38,8 +45,12 @@ bool hit_sphere(const point3 &center, double radius, const ray &r) {
     @return color of the ray
 */
 color ray_color(const ray &r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    // calculate the sphere normal 
+    // from the center of the sphere to the hit point
+    double t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -87,11 +98,16 @@ int main() {
     for (int i = 0; i < image_height; i++) {
         std::clog << "\rScanlines remaining: " << (image_height - i) << " " << std::flush;
         for (int j = 0; j < image_width; j++) {
+            // get center of pixel based on pixel deltas
             point3 pixel_center = pixel00_location + (j * pixel_delta_u) + (i * pixel_delta_v);
+            // get direction of the vector camera to pixel center
             vec3 ray_direction = pixel_center - camera_center;
+            // get ray from camera center to the pixel center
             ray r(camera_center, ray_direction);
-
+            
+            // calculate color of the ray
             color pixel_color = ray_color(r);
+            // write color in ppm format
             write_color(std::cout, pixel_color);
         }
     }
