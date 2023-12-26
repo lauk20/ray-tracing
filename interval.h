e30d12b45fd6dd287d12a2ede6d7019ec7d8f629
Kenny <72945813+lauk20@users.noreply.github.com>
2023-12-26 17:05:56 -0500

    add clamp function to clamp to interval

--- interval.h
+++ interval.h
@@ -37,6 +37,20 @@ class interval {
             return min < x && x < max;
         }
 
+        /*
+            mathematical clamp function that clamps to min, max
+
+            @param x number to clamp
+
+            @return the clamped number
+        */
+        double clamp(double x) const {
+            if (x < min) return min;
+            if (x > max) return max;
+
+            return x;
+        }
+
         static const interval empty, universe;
 };
 
