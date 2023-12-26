36eab73da775a3893b60f7ac8a74546a0a653023
Kenny <72945813+lauk20@users.noreply.github.com>
2023-12-26 17:03:37 -0500

    add functions to generate random doubles

--- constants.h
+++ constants.h
@@ -2,6 +2,7 @@
 #define CONSTANTS_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -17,6 +18,24 @@ const double pi = 3.1415926535897932385;
 */
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+/*
+    get random double in [0, 1)
+
+    @return double in [0, 1)
+*/
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+/*
+    get random double in [min, max)
+
+    @return double in [min, max)
+*/
+inline double random_double(double min, double max) {
+    return min + (max-min) * random_double();
 }
 
 #include "interval.h"
