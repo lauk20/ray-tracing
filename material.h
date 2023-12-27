d1ec67b41f1c6c9ed55584e984b4be2cace2c7ad
Kenny <72945813+lauk20@users.noreply.github.com>
2023-12-27 17:16:06 -0500

    add metal material

--- material.h
+++ material.h
@@ -6,15 +6,33 @@
 #include "hittable.h"
 #include "vec3.h"
 
+/*
+    class representing a material
+*/
 class material {
     public:
         virtual ~material() = default;
 
+        /*
+            ray scattering/bouncing for the material
+            modifies rec, attenuation, and scattered parameters.
+
+            @param r_in the ray
+            @param rec the hit record to modify
+            @param attenuation how much color each bounce should retain
+            @param scattered the scattered ray
+
+            @return true on success
+        */
         virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const = 0;
 };
 
+/*
+    class representing a diffuse material
+*/
 class lambertian : public material {
     public:
+        // constructor
         lambertian(const color &a) : albedo(a) {}
 
         bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
@@ -31,6 +49,28 @@ class lambertian : public material {
         }
     
     private:
+        // how much color each ray bounce should retain
+        color albedo;
+};
+
+/*
+    class representing metal material
+*/
+class metal : public material {
+    public:
+        // constructor
+        metal(const color &a) : albedo(a) {}
+
+        bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
+            attenuation = albedo;
+
+            return true;
+        }
+    
+    private:
+        // how much color each ray bounce should retain
         color albedo;
 };
 
